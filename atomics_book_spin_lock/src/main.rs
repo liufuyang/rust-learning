4b64f6739bc6cbf529cf30e87d69684abc9e2209
Fuyang Liu <liufuyang@users.noreply.github.com>
2023-11-25 16:20:32 +0100

    remove lazy_statics but use scoped threads

--- atomics_book_spin_lock/src/main.rs
+++ atomics_book_spin_lock/src/main.rs
@@ -1,46 +1,47 @@
 use std::ops::{Deref, DerefMut};
 use std::thread;
 use atomics_book_spin_lock::spin_lock::r#unsafe::SpinLockUnsafe;
-use lazy_static::lazy_static;
 use atomics_book_spin_lock::spin_lock::safe::SpinLock;
 
-lazy_static! {
-    static ref LOCK_UNSAFE: SpinLockUnsafe<i32> = {
-        SpinLockUnsafe::new(0)
-    };
-
-    static ref LOCK: SpinLock<i32> = {
-        SpinLock::new(0)
-    };
-}
-
 fn main() {
-    println!("Hello, world!");
+    let lock_unsafe = SpinLockUnsafe::new(0);
+    let lock = SpinLock::new(0);
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| {
+            let v = lock_unsafe.lock();
+            *v = *v + 1;
+            println!("A - lock_unsafe - v:{}", *v);
+            unsafe {lock_unsafe.unlock()}
+            *v = *v + 1; // Very unsafe as other thread will either read before or after this happens.
 
-    let t1 = thread::spawn(move || {
-        let v = LOCK_UNSAFE.lock();
-        *v = *v + 1;
-        println!("LOCK_UNSAFE - v:{}", *v);
-        unsafe {LOCK_UNSAFE.unlock()}
-        *v = *v + 1; // Very unsafe as other thread will either read before or after this happens.
+            // safe lock usage
+            let mut v = lock.lock();
+            *v.deref_mut() = v.deref() + 1;
+            println!("A - lock - v:{}", *v);
 
-        // safe lock usage
-        let mut v = LOCK.lock();
-        *v.deref_mut() = v.deref() + 1;
-        println!("lock - v:{}", *v);
+            // another safe lock usage
+            x.lock().push(1)
+        });
+
+        s.spawn(|| {
+            let v = lock_unsafe.lock();
+            *v = *v + 1;
+            println!("B - lock_unsafe - v:{}", *v); // can be 2 or 3.
+            unsafe {lock_unsafe.unlock()}
+
+            // safe lock usage
+            let mut v = lock.lock();
+            *v.deref_mut() = v.deref() + 1;
+            println!("B - lock - v:{}", *v);
+
+            // another safe lock usage
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
     });
-    let t2 = thread::spawn(move || {
-        let v = LOCK_UNSAFE.lock();
-        *v = *v + 1;
-        println!("l1 - v:{}", *v);
-        unsafe {LOCK_UNSAFE.unlock()}
 
-        // safe lock usage
-        let mut v = LOCK.lock();
-        *v.deref_mut() = v.deref() + 1;
-        println!("lock - v:{}", *v);
-    });
-
-    t1.join().unwrap();
-    t2.join().unwrap();
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
 }
